ff764b99e36b8edf28314cc4723cbdbff1132e96
baikil <78218286+baikil@users.noreply.github.com>
2024-03-23 18:15:05 -0400

    Add files via upload

--- T2.cmd
+++ T2.cmd
@@ -1,23 +1,29 @@
 @echo off
 ::App version
-set appv=0.2
+set appv=0.3
 ::Verify if T2 is has admin privileges and if not, prompt to restart with them
-net.exe session 1>NUL 2>NUL || (
+net.exe session 1>nul 2>nul || (
 	title T2 - WARNING : Not admin
 	echo [33m[WARNING][0m[1m T2 is not currently running as admin and some commands might not work[0m
-	echo           Do you want to restart it as admin^? [Y/n]
+	echo           Do you want to restart it as admin^? [y/N]
 	choice /c yn /n)
+
 if %errorlevel% == 1 (
 	powershell start -verb runas '%0'
 	exit /b)
+::Set the function shortcut
+set "f=call :"
+::Set T2's data folder path
+for %%a in (%0) do (set "t2data=%%~da%%~pa")
 
 :00 Menu
 cls&title T2 - Menu
 echo [1mTroubleshooting Tools (T2)[0m ver.%appv%&echo.
 echo 1. Network
-echo 2. More
-echo 3. Exit
-choice /c 123 /n
+echo 2. Storage
+echo 3. More
+echo 4. Exit
+choice /c 1234 /n
 goto %errorlevel%0
 
 :10 Network
@@ -47,7 +53,7 @@ goto 11%errorlevel%
 cls&title T2 - Restarting the "Wi-Fi" network adapter...
 echo Restarting the "Wi-Fi" network adapter...
 powershell -command "(Restart-NetAdapter -Name "Wi-Fi")"
-echo.&echo [93m[Done][0m
+echo.&echo [92m[Done][0m
 pause
 goto 11
 
@@ -55,7 +61,7 @@ goto 11
 cls&title T2 - Restarting the "Ethernet" network adapter...
 echo Restarting the "Ethernet" network adapter...
 powershell -command "(Restart-NetAdapter -Name "Ethernet")"
-echo.&echo [93m[Done][0m
+echo.&echo [92m[Done][0m
 pause
 goto 11
 
@@ -82,7 +88,7 @@ title T2 - Trying to restart the "%awa%*" network adapter...
 pause
 powershell -command "(Restart-NetAdapter -Name "%awa%^*")"
 )
-echo.&echo [93m[Done][0m
+echo.&echo [92m[Done][0m
 pause
 goto 11
 
@@ -99,11 +105,37 @@ nbtstat -R
 nbtstat -RR
 ipconfig /flushdns
 ipconfig /registerdns
-echo.&echo [93m[Done][0m
+echo.&echo [92m[Done][0m
 pause
 goto 10
 
-:20 More
+:20 Storage
+cls&title T2 - Storage tools
+echo [1mStorage tools (T2)[0m&echo.
+%f%getFolderSize %temp%
+echo 1. Clean %%temp%% (%gfs% bytes)
+echo 2. (^<-)
+choice /c 12 /n
+goto 2%errorlevel%
+
+:21 Clean %temp%
+cls&title T2 - Clean %%temp%%
+echo [1mClean %%temp%% (T2)[0m&echo.
+%f%getFolderSize %temp%
+set bef=%gfs%
+echo [33m[Deleting][0m
+del /f /s /q %temp%\*.*>nul
+%f%getFolderSize %temp%
+echo.&echo [92m[Done][0m
+%f%math %bef%-%gfs%
+echo %math% bytes cleared
+echo.&pause
+goto 20
+
+:22 Go back
+goto 00
+
+:30 More
 cls&title T2 - More
 echo [1mTroubleshooting Tools (T2)[0m ver.%appv%&echo.
 echo       -About-
@@ -116,11 +148,34 @@ echo 2. (^<-)
 choice /c 12 /n
 goto 2%errorlevel%
 
-:21 Restart as admin
+:31 Restart as admin
 powershell start -verb runas '%0'&exit/b
 
-:22 Go back
+:32 Go back
 goto 00
 
-:30 Exit
+:40 Exit
 exit
+
+:::::::::::::::
+:: Functions ::
+:::::::::::::::
+
+:getFolderSize <Path> [->] %gfs%
+setlocal enableextensions disabledelayedexpansion
+set "target=%~1"
+if not defined target set "target=%cd%"
+set "size=0"
+for /f "tokens=3,5" %%a in ('
+dir /a /s /w /-c "%target%"
+^| findstr /b /l /c:"  "
+') do if "%%b"=="" set "size=%%a"
+echo %size%>getFolderSize
+endlocal
+set /p gfs=<getFolderSize
+del getFolderSize
+exit /b
+
+:math <Equation> [->] %math%
+for /f "tokens=* usebackq" %%M in (`%t2data%calc.exe %1`) do (set math=%%M)
+exit /b
